1e50474581580fa44424780dd8ebcc9f89f57af6
Matej Kosec <mkosec@stanford.edu>
2018-03-15 18:33:04 -0700

    Bug fix1

--- code/qa_model.py
+++ code/qa_model.py
@@ -30,7 +30,7 @@ from tensorflow.python.ops import embedding_ops
 from evaluate import exact_match_score, f1_score
 from data_batcher import get_batch_generator
 from pretty_print import print_example
-from modules import RNNEncoder, SimpleSoftmaxLayer, BasicAttn, masked_softmax, MultiplicativeAttn
+from modules import RNNEncoder, SimpleSoftmaxLayer, BasicAttn, masked_softmax
 
 logging.basicConfig(level=logging.INFO)
 
