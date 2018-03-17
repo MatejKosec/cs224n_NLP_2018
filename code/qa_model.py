5fae90d45882b60f17d560a5f4edb3a2a42a42dd
Matej Kosec <mkosec@stanford.edu>
2018-03-17 00:02:32 -0700

    Implement better searching 3

--- code/qa_model.py
+++ code/qa_model.py
@@ -415,7 +415,7 @@ class QAModel(object):
         for batch in get_batch_generator(self.word2id, context_path, qn_path, ans_path, self.FLAGS.batch_size, context_len=self.FLAGS.context_len, question_len=self.FLAGS.question_len, discard_long=False):
 
             pred_start_pos, pred_end_pos = self.get_start_end_pos(session, batch)
-            #print 'Pred start pos', pred_start_pos, ', pred end pos', pred_end_pos
+            print 'Pred start pos', pred_start_pos, ', pred end pos', pred_end_pos
 
             # Convert the start and end positions to lists length batch_size
             pred_start_pos = pred_start_pos.tolist() # list length batch_size
